8ce0be459cd23a7e75e16de1a46f1ac99e254df4
CarlosCo_Dev <condorpasa26@gmail.com>
2024-05-15 10:30:24 -0500

    Delete server/dist directory

--- server/dist/helpers/token.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genToken = void 0;
-const genToken = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
-exports.genToken = genToken;
